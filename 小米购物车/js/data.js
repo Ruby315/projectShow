855b0f92a0619c125691db1c2ab7610fa2cc00a7
在厕所写代码的那个雪夜 <128269425+Ruby315@users.noreply.github.com>
2025-07-02 20:23:14 +0800

    Add files via upload

---
+++ 小米购物车/js/data.js
@@ -0,0 +1,129 @@
+var shopcarts = [{
+		// 购物车编号
+		cartId: 1,
+		// 店铺编号 
+		// shopId: 2001,
+		// // 店铺名称
+		// shopName: "荣耀官方旗舰店",
+		// isChecked: false,
+		products: [{
+				// 商品编号
+				productId: 1001,
+				// 商品名称
+				productName: "小米手环7 夜跃黑",
+				// 顶层图片
+				topImg: "img/005.jpg",
+				// 是否参加电竞节
+				isActivity: true,
+				// 是否支持信用卡
+				isCard: true,
+				// 是否支持七天退货
+				isSeven: true,
+				// 是否如实描述
+				isRealDesc: true,
+				// 是否选择
+				isChecked: false,
+				// 单价
+				price: 209.00,
+				// 购买的数量
+				count: 1,
+				// 金额
+				amount: 209.00
+			},
+			{
+				// 商品编号
+				productId: 1002,
+				// 商品名称
+				productName: "Redmi K60 12GB+512GB 墨羽",
+				// 顶层图片
+				topImg: "img/004.jpg",
+				
+				// 是否参加电竞节
+				isActivity: true,
+				// 是否支持信用卡
+				isCard: true,
+				// 是否支持七天退货
+				isSeven: true,
+				// 是否如实描述
+				isRealDesc: true,
+				// 是否选择
+				isChecked: true,
+				// 单价
+				price: 2999.00,
+				// 购买的数量
+				count: 1,
+				// 金额
+				amount: 2999.00
+			}
+		]
+	},
+	{
+		// 购物车编号
+		cartId: 2,
+		// // 店铺编号 
+		// shopId: 2002,
+		// // 店铺名称
+		// shopName: "罗蒙派大星专卖店",
+		// isChecked: false,
+		products: [{
+			// 商品编号
+			productId: 1003,
+			// 商品名称
+			productName: "小米小爱随身音箱 白色",
+			// 顶层图片
+			topImg: "img/002.jpg",
+			
+			// 是否参加电竞节
+			isActivity: false,
+			// 是否支持信用卡
+			isCard: true,
+			// 是否支持七天退货
+			isSeven: true,
+			// 是否如实描述
+			isRealDesc: true,
+			// 是否选择
+			isChecked: false,
+			// 单价
+			price: 49.00,
+			// 购买的数量
+			count: 2,
+			// 金额
+			amount: 98.00
+		}]
+	},
+	{
+		// 购物车编号
+		cartId: 3,
+		// // 店铺编号 
+		// shopId: 2003,
+		// // 店铺名称
+		// shopName: "雅鹿提趣专卖店",
+		// isChecked: true,
+		products: [{
+			// 商品编号
+			productId: 1004,
+			// 商品名称
+			productName: "Redmi 10A 4GB+64GB 烟波蓝",
+			// 顶层图片
+			topImg: "img/001.jpg",
+			
+			// 是否参加电竞节
+			isActivity: false,
+			// 是否支持信用卡
+			isCard: true,
+			// 是否支持七天退货
+			isSeven: true,
+			// 是否如实描述
+			isRealDesc: true,
+			// 是否选择
+			isChecked: true,
+			
+			// 单价
+			price: 649.00,
+			// 购买的数量
+			count: 1,
+			// 金额
+			amount: 649.00
+		}]
+	}
+]
